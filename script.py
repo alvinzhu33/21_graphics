77d63275694b0a2d8a1e3b674586a3bd03009cd3
Alvin Zhu <alvinzhu33@yahoo.com>
2017-06-13 02:23:05 -0400

    REVERTING

--- script.py
+++ script.py
@@ -4,20 +4,16 @@ from matrix import *
 from draw import *
 
 """======== first_pass( commands, symbols ) ==========
-
   Checks the commands array for any animation commands
   (frames, basename, vary)
 
   Should set num_frames and basename if the frames
   or basename commands are present
-
   If vary is found, but frames is not, the entire
   program should exit.
-
   If frames is found, but basename is not, set name
   to some default value, and print out a message
   with the name being used.
-
   jdyrlandweaver
   ==================== """
 def first_pass( commands ):
@@ -48,17 +44,14 @@ def first_pass( commands ):
     return (name, num_frames)
 
 """======== second_pass( commands ) ==========
-
   In order to set the knobs for animation, we need to keep
   a seaprate value for each knob for each frame. We can do
   this by using an array of dictionaries. Each array index
   will correspond to a frame (eg. knobs[0] would be the first
   frame, knobs[2] would be the 3rd frame and so on).
-
   Each index should contain a dictionary of knob values, each
   key will be a knob name, and each value will be the knob's
   value for that frame.
-
   Go through the command array, and when you find vary, go
   from knobs[0] to knobs[frames-1] and add (or modify) the
   dictionary corresponding to the given knob with the
@@ -66,10 +59,6 @@ def first_pass( commands ):
   ===================="""
 def second_pass( commands, num_frames ):
     frames = [ {} for i in range(num_frames) ]
-    cons = {};
-    sources = {};
-    amb = {};
-    shading = "none"
 
     for command in commands:
         if command[0] == 'vary':
@@ -120,9 +109,6 @@ def run(filename):
     step = 0.1
 
     #print symbols
-    sources = [symbols[i][1] for i in symbols if symbols[i][0] == "light"]
-    if "shading" in symbols:
-        shading = symbols["shading"][1];
 
     for f in range(num_frames):
 
@@ -155,25 +141,22 @@ def run(filename):
                         symbols[knob][1] = args[0]
             elif c == 'box':
                 add_box(tmp,
-                        args[0+1], args[1+1], args[2+1],
-                        args[3+1], args[4+1], args[5+1])
-                color = args[0]
+                        args[0], args[1], args[2],
+                        args[3], args[4], args[5])
                 matrix_mult( stack[-1], tmp )
-                draw_polygons(tmp, screen, zb, color, symbols[command[-1]][1], sources, shading) if command[-1] else draw_polygons(tmp, screen, zb, color);
+                draw_polygons(tmp, screen, zb, color)
                 tmp = []
             elif c == 'sphere':
                 add_sphere(tmp,
-                           args[0+1], args[1+1], args[2+1], args[3+1], step)
-                color = args[0]
+                           args[0], args[1], args[2], args[3], step)
                 matrix_mult( stack[-1], tmp )
-                draw_polygons(tmp, screen, zb, color, symbols[command[-1]][1], sources, shading) if command[-1] else draw_polygons(tmp, screen, zb, color);
+                draw_polygons(tmp, screen, zb, color)
                 tmp = []
             elif c == 'torus':
                 add_torus(tmp,
-                          args[0+1], args[1+1], args[2+1], args[3+1], args[4+1], step)
-                color = args[0]
+                          args[0], args[1], args[2], args[3], args[4], step)
                 matrix_mult( stack[-1], tmp )
-                draw_polygons(tmp, screen, zb, color, symbols[command[-1]][1], sources, shading) if command[-1] else draw_polygons(tmp, screen, zb, color);
+                draw_polygons(tmp, screen, zb, color)
                 tmp = []
             elif c == 'move':
                 if command[-1]:
